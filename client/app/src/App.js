26ae38521dafe39d6de922288a609bc727cf82b5
Sara Shah Baig <sarashahbaig@gmail.com>
2019-12-30 16:04:46 -0800

    server and client setup

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,12 +1,65 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <h1>Mezbaan</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://127.0.0.1:5000/")
+      .then(res => res.data)
+      .then(data => {
+        console.log(data[0]);
+        this.setState({ users: data[0] });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  handleLogin = () => {
+    axios
+      .get("http://127.0.0.1:5000/login")
+      .then(res => res.data)
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  handleLogout = () => {
+    axios
+      .get("http://127.0.0.1:5000/logout")
+      .then(res => res.data)
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { users } = this.state;
+    return (
+      <div>
+        <h1>Mezbaan</h1>
+        {users.map((user, index) => (
+          <h3 key={index}>{user.name}</h3>
+        ))}
+        <button onClick={this.handleLogin}>Login</button>
+        <button onClick={this.handleLogout}>Logout</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- server/app.py
+++ server/app.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from flask import jsonify
 from flask import make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
-def home():
-  return make_response({"res": "change"}, 200)
+def index():
+  users = [{"name": "John", "age": 22}, {"name": "Sam", "age": 31}]
+  return jsonify(users, 200)
 
 @app.route("/login")
 def login():
   user = {"name": "Jon", "age": 22}
   return jsonify(user)
+
+@app.route("/logout")
+def logout():
+  return "You are now logged out"
 
 if __name__ == "__main__":
   app.run(debug=True)
