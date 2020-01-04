fe1735352192d2e0eb8f09df194f6a04e636ad73
Sara Shah Baig <sarashahbaig@gmail.com>
2020-01-03 21:02:08 -0800

    added constants in a file

---
+++ client/app/src/constants.js
@@ -0,0 +1,15 @@
+export const API_ROUTES = {
+  main: "http://127.0.0.1:5000/api",
+  users: "/users",
+  register: "/register",
+  login: "/login"
+};
+export const MENU_ITEMS = [
+  { name: "Login", link: "/login" },
+  { name: "Register", link: "/register" }
+];
+
+export const HOME = {
+  name: "Mezbaan",
+  link: "/"
+};
