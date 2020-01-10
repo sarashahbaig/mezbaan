550f76d3ea8473ece9a9fec53ee16918887b4c2c
Sara Shah Baig <sarashahbaig@gmail.com>
2020-01-09 19:16:52 -0800

    Updates

---
+++ precache-manifest.f81b67621dfd854b08cf32d1bc695d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ed692a3d6d51c06d3473911e9b70ff",
+    "url": "/mezbaan/index.html"
+  },
+  {
+    "revision": "b1a968e7adac1644a245",
+    "url": "/mezbaan/static/css/main.bdc6506f.chunk.css"
+  },
+  {
+    "revision": "e23164d680691e8c5501",
+    "url": "/mezbaan/static/js/2.c2a702ec.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/mezbaan/static/js/2.c2a702ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1a968e7adac1644a245",
+    "url": "/mezbaan/static/js/main.51eb4f02.chunk.js"
+  },
+  {
+    "revision": "c80eb6fa221849c788fc",
+    "url": "/mezbaan/static/js/runtime-main.c8deaa77.js"
+  }
+]);
