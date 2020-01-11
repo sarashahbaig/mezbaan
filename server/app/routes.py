c576d6d8dbdb4a617a0bab7db0c5fa49fef44a54
Sara Shah Baig <sarashahbaig@gmail.com>
2020-01-10 23:11:14 -0800

    added a route to get languages

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, make_response, abort
 from app import app, db, bcrypt
-from app.models import User
+from app.models import User, Language
 
 
 @app.route("/", methods=["GET"])
@@ -11,20 +11,54 @@ def index():
 @app.route("/api/register", methods=["POST"])
 def register():
 
+  is_volunteer = request.json.get('isVolunteer')
   username = request.json.get('username')
   email = request.json.get('email')
   password = request.json.get('password')
-  hours = request.json.get('hours')
-  days_can_volunteer = request.json.get('days_can_volunteer')
-  time_can_volunteer = request.json.get('time_can_volunteer')
+  first_name = request.json.get('firstName')
+  last_name = request.json.get('lastName')
+  city = request.json.get('city')
+  state = request.json.get('state')
+  zip_code = request.json.get('zipCode')
   languages = request.json.get('languages')
+  tasks = request.json.get('tasks')
+  days_can_volunteer = request.json.get('days')
+  time_can_volunteer = request.json.get('time_can_volunteer')
+  description = request.json.get('description')
+
+ 
+
+  # return jsonify(respn)
 
   if username is None or email is None or password is None:
       return make_response("One of the fields is missing", 400)
 
   pwd_hash = bcrypt.generate_password_hash(password)
 
-  user = User(username=username, email=email, password_hash=pwd_hash, hours=hours, days_can_volunteer=days_can_volunteer, time_can_volunteer=time_can_volunteer, languages=languages)
+  user_languages = []
+  for lang_id in languages:
+    language = Language.query.filter_by(id=lang_id).first()
+    user_languages.append(language)
+  
+
+  user_data = {
+    "username": username,
+    "password_hash": pwd_hash,
+    "email": email,
+    "first_name": first_name,
+    "last_name": last_name,
+    "city": city,
+    "state": state,
+    "zip_code": zip_code,
+    "languages": user_languages,
+    "description": description,
+    "is_volunteer": is_volunteer,
+    "tasks": tasks,
+    "days_can_volunteer": days_can_volunteer,
+    "time_can_volunteer": time_can_volunteer,
+  }
+
+  user = User(*user_data.values())
   db.session.add(user)
   db.session.commit()
 
@@ -64,6 +98,14 @@ def get_all_users():
   return jsonify(users)
   # i.serialize for i in qryresult.all()]
 
-  @app.route("/api/afterlogin", methods=["GET"])
-  def afterlogin():
-    return "hello"
+@app.route("/api/afterlogin", methods=["GET"])
+def afterlogin():
+  return "hello"
+
+
+@app.route('/api/languages', methods=['GET'])
+def get_languages():
+  # languages = Language.query.all()
+  languages = [lang.to_json() for lang in Language.query]
+  return jsonify(languages)
+
