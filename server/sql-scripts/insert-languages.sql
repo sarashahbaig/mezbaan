9c5d5b1b8eac1141b3db84a07207bd175c1ad544
Sara Shah Baig <sarashahbaig@gmail.com>
2020-01-10 23:11:37 -0800

    added sql script for easy data generation

---
+++ server/sql-scripts/insert-languages.sql
@@ -0,0 +1,5 @@
+INSERT INTO `language`(`language`) VALUES ('English');
+INSERT INTO `language`(`language`) VALUES ('Baloch');
+INSERT INTO `language`(`language`) VALUES ('Urdu');
+INSERT INTO `language`(`language`) VALUES ('Arabic');
+INSERT INTO `language`(`language`) VALUES ('Russian');
