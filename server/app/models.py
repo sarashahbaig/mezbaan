48f9e387af1bad8c526a6dd79e0c2bc5ea673e68
Sara Shah Baig <sarashahbaig@gmail.com>
2020-01-13 22:07:29 -0800

    added models for Language and Service

--- server/app/models.py
+++ server/app/models.py
@@ -4,8 +4,15 @@ from app import db
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-languages = db.Table('user_languages', 
+
+languages = db.Table('user_languages',
 db.Column('language_id', db.Integer, db.ForeignKey('language.id'), primary_key=True),
+db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
+)
+
+
+services = db.Table('user_services',
+db.Column('service_id', db.Integer, db.ForeignKey('service.id'), primary_key=True),
 db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
 )
 
@@ -15,16 +22,17 @@ class Language(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     language = db.Column(db.String(128))
 
-
-    @property
-    def serialize(self):
-        return {
-             "id": self.id,
-            "language": self.language
-        }
     def to_json(self):
         return dict(id=self.id, language=self.language)
 
+class Service(db.Model):
+    _tablename__ = "service"
+
+    id = db.Column(db.Integer, primary_key=True)
+    service = db.Column(db.String(128))
+
+    def to_json(self):
+        return dict(id=self.id, service=self.service)
 
 class User(db.Model):
     __tablename__ = "user"
@@ -36,19 +44,18 @@ class User(db.Model):
     city = db.Column(db.String(128))
     state = db.Column(db.String(128))
     zip_code = db.Column(db.String(128))
-    languages = db.relationship('Language', secondary=languages, lazy='subquery', backref=db.backref('user', lazy=True))
+    languages = db.relationship('Language', secondary=languages, lazy='subquery', backref=db.backref('user', lazy=True, uselist=False))
+    services = db.relationship('Service', secondary=services, lazy= 'subquery', backref=db.backref('user', lazy=True, uselist=False))
     description = db.Column(db.String(500))
-    tasks = db.Column(db.String(128))
     password_hash = db.Column(db.String(200))
     is_voluteer = db.Column(db.Boolean, default=False)
     days_can_volunteer = db.Column(db.String(10))
-    time_can_volunteer = db.Column(db.Integer)
-    
+    time_can_volunteer = db.Column(db.Integer)    
     
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {>'.format(self.username)
     
-    def __init__(self, username, password_hash, email, first_name, last_name, city , state, zip_code, languages, description, is_voluteer, tasks, days_can_volunteer, time_can_volunteer ):
+    def __init__(self, username, password_hash, email, first_name, last_name, city , state, zip_code, languages, description, is_voluteer, services, days_can_volunteer, time_can_volunteer ):
         self.username = username
         self.password_hash = password_hash 
         self.email = email 
@@ -60,27 +67,10 @@ class User(db.Model):
         self.languages = languages 
         self.description = description
         self.is_voluteer = is_voluteer
-        self.tasks = tasks 
+        self.services = services
         self.days_can_volunteer = days_can_volunteer
-        self.time_can_volunteer = time_can_volunteer 
-        
-    @property
-    def serialize(self):
-        return {
-            "id": self.id,
-            "username": self.username,
-            "email": self.email,
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "city": self.city,
-            "state": self.state,
-            "zip_code": self.zip_code,
-            "languages": self.languages,
-            "description": self.description,
-            "tasks": self.tasks,
-            "password_hash": self.password_hash,
-            "days_can_volunteer": self.days_can_volunteer,
-            "time_can_voluntee": self.time_can_volunteer
-        }
+        self.time_can_volunteer = time_can_volunteer
 
-
+    def to_json(self):
+        return dict(id=self.id, username=self.username, password_hash=self.password_hash, email=self.email, first_name=self.first_name, last_name=self.last_name, city=self.city, state=self.state, zip_code=self.zip_code, languages=[language.to_json() for language in self.languages], description=self.description, is_voluteer=self.is_voluteer, services=[service.to_json() for service in self.services], days_can_volunteer=self.days_can_volunteer, time_can_volunteer=self.time_can_volunteer
+        )
