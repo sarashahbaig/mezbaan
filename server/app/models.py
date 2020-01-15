b1d38fd3a0a3cf143b96a605c95dafae7d7a061f
Sara Shah Baig <sarashahbaig@gmail.com>
2020-01-14 23:38:11 -0800

    added to_json method

--- server/app/models.py
+++ server/app/models.py
@@ -48,14 +48,14 @@ class User(db.Model):
     services = db.relationship('Service', secondary=services, lazy= 'subquery', backref=db.backref('user', lazy=True, uselist=False))
     description = db.Column(db.String(500))
     password_hash = db.Column(db.String(200))
-    is_voluteer = db.Column(db.Boolean, default=False)
+    is_volunteer = db.Column(db.Boolean, default=False)
     days_can_volunteer = db.Column(db.String(10))
     time_can_volunteer = db.Column(db.Integer)    
     
     def __repr__(self):
         return '<User {>'.format(self.username)
     
-    def __init__(self, username, password_hash, email, first_name, last_name, city , state, zip_code, languages, description, is_voluteer, services, days_can_volunteer, time_can_volunteer ):
+    def __init__(self, username, password_hash, email, first_name, last_name, city , state, zip_code, languages, description, is_volunteer, services, days_can_volunteer="", time_can_volunteer=0):
         self.username = username
         self.password_hash = password_hash 
         self.email = email 
@@ -66,11 +66,25 @@ class User(db.Model):
         self.zip_code = zip_code 
         self.languages = languages 
         self.description = description
-        self.is_voluteer = is_voluteer
+        self.is_volunteer = is_volunteer
         self.services = services
         self.days_can_volunteer = days_can_volunteer
         self.time_can_volunteer = time_can_volunteer
 
     def to_json(self):
-        return dict(id=self.id, username=self.username, password_hash=self.password_hash, email=self.email, first_name=self.first_name, last_name=self.last_name, city=self.city, state=self.state, zip_code=self.zip_code, languages=[language.to_json() for language in self.languages], description=self.description, is_voluteer=self.is_voluteer, services=[service.to_json() for service in self.services], days_can_volunteer=self.days_can_volunteer, time_can_volunteer=self.time_can_volunteer
-        )
+        return  { "id": self.id,
+            "username": self.username,
+            "email": self.email,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "city": self.city,
+            "state": self.state,
+            "zipCode": self.zip_code,
+            "languages": [language.to_json() for language in self.languages],
+            "description": self.description,
+            "isVolunteer": self.is_volunteer,
+            "services": [service.to_json() for service in self.services],
+            "daysCanVolunteer": self.days_can_volunteer,
+            "timeCanVolunteer": self.time_can_volunteer 
+            }
+        
