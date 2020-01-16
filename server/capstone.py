934c9b47c8ab34a953b050fd024177014898d721
Sara Shah Baig <sarashahbaig@gmail.com>
2020-01-16 11:19:59 -0800

    added rating

--- server/capstone.py
+++ server/capstone.py
@@ -1,7 +1,7 @@
 from app import app, db
-from app.models import User, Language, Service
+from app.models import User, Language, Service, Rating
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Language': Language, 'Service': Service }
+    return {'db': db, 'User': User, 'Language': Language, 'Service': Service, 'Rating': Rating }
