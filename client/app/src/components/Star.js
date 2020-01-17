cd1990de682f167f50beb8718560f6d988ab953e
Sara Shah Baig <sarashahbaig@gmail.com>
2020-01-16 22:35:03 -0800

    added star component

---
+++ client/app/src/components/Star.js
@@ -0,0 +1,14 @@
+import React from "react";
+import star from "../images/icon/star.svg";
+
+class Star extends React.Component {
+  render() {
+    return (
+      <div>
+        <img className="icon-star" src={star} />
+      </div>
+    );
+  }
+}
+
+export default Star;
