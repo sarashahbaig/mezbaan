0b236f84197a3476d69339e164ba1fb75024b28e
Sara Shah Baig <sarashahbaig@gmail.com>
2020-01-18 16:43:29 -0800

    created day function

--- server/load_data.py
+++ server/load_data.py
@@ -1,6 +1,19 @@
 import csv
 from app import app, db, bcrypt
-from app.models import User, Language, Service, Rating
+from app.models import User, Language, Service, Rating, Day
+from app.routes import to_date
+
+
+def db_create_days():
+    with open('mockdata/days.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            for val in row:
+                day = Day(day=val)
+                db.session.add(day)
+                db.session.commit()
+
+
 
 def db_create_language():
     with open('mockdata/languages.csv') as csv_file:
@@ -41,32 +54,46 @@ def db_create_user():
             user_languages = []
             user_services = []
             user_ratings = []
+            user_days = []
             for col in row:
                 if column == 9:
                     col = bool(col)
-                if column == 10 or column == 11 or col == 12:
-
+                print("COLUMN+++++++++++++++++++++++++++++++++")
+                print(column)
+                print(col)
+                if column == 10:
                     col = [int(s) for s in str(col).split()]
-                    
-                    if column == 10:
-                        for lang_id in col:
-                            language = Language.query.filter_by(id=lang_id).first()
-                            user_languages.append(language)
-                    
-                    if column == 11:
-                        for serv_id in col:
-                            service = Service.query.filter_by(id=serv_id).first() 
-                            user_services.append(service)
-                    if column == 12:
-                        for rating_id in col:
-                            rating = Rating.query.filter_by(id=rating_id).first() 
-                            user_ratings.append(rating)
-
+                    for lang_id in col:
+                        language = Language.query.filter_by(id=lang_id).first()
+                        user_languages.append(language)
                 
+                if column == 11:
+                    col = [int(s) for s in str(col).split()]
+                    for serv_id in col:
+                        service = Service.query.filter_by(id=serv_id).first() 
+                        user_services.append(service)
+                print("COLUMN@+++++++++++++++++++++++++++++++++")
+                print(column)
+                print(col)
+                if column == 12:
+                    col = [int(s) for s in str(col).split()]
+                    for rating_id in col:
+                        rating = Rating.query.filter_by(id=rating_id).first() 
+                        user_ratings.append(rating)
+                    print("RATINGS+++++++++++++++++++++++++++++++++")
+                    print(user_ratings)
+                    print("RATINGS+++++++++++++++++++++++++++++++++")
+
+                if column == 13:
+                    col = [int(s) for s in str(col).split()]
+                    for day_id in col:
+                        day = Day.query.filter_by(id=day_id).first() 
+                        user_days.append(day)
+
                 if column == 14:
                     
-                    col = int(col)
-                    print("+++++++++++++++++++++++++++++++++")
+                    col = to_date(col)
+                    
 
                 key = keys[column]
                 user_data[key] = col
@@ -75,6 +102,7 @@ def db_create_user():
             user_data["languages"] = user_languages
             user_data["services"] = user_services
             user_data["ratings"] = user_ratings
+            user_data["days_can_volunteer"] = user_days 
             password = user_data["password_hash"]
             user_data["password_hash"] = bcrypt.generate_password_hash(password)
 
@@ -89,6 +117,7 @@ def db_create_user():
 
 
 def main():
+    db_create_days()
     db_create_language()
     db_create_service()
     db_create_rating()
